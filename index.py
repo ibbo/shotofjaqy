a919862e209b9d87df6ea551ab78ab884290cef5
Thomas Ibbotson <thomas.ibbotson@gmail.com>
2010-02-27 10:35:38 +0000

    Added a home page and license file.
    Home page can now be found at http://shotofjaqybot.appspot.com.

---
+++ index.py
@@ -0,0 +1,26 @@
+print "Content-type: text/html"
+print
+print """
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<body>
+<h1>Shot of Jaqy</h1>
+<p>The Shot of Jaqy wave gadget and robot were created to embed the
+<a href="http://www.shotofjaq.org">Shot of Jaq</a> rss feed into a wave.
+You can see the embedded gadget in the Shot of Jaq
+<a href="https://wave.google.com/wave/#restored:wave:googlewave.com!w%252BzCvDkNZdF">
+public wave</a>.
+The gadget xml file currently resides at
+<a href="http://shotofjaqybot.appspot.com/assets/shotofjaqy.xml">
+http://shotofjaqybot.appspot.com/assets/shotofjaqy.xml
+</a>
+which you can use to embed into your own wave.</p>
+<p>The robot isn't functional yet. The idea is to have it update the wave
+with new shots and comments as they come in.</p>
+<p>The source code is licensed with an MIT license and can be found at
+<a href="http://github.com/ibbo/shotofjaqy">github</a>.</p>
+<p>Don't forget to check out
+<a href="http://www.shotofjaq.org">the Shot of Jaq website</a>.
+</p>
+</body>"""
